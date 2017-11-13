812773a47fe43849aa75d8fb463b132238685c9b
HDCarBoNe <dylan.gogeta@gmail.com>
2017-11-13 01:16:03 +0100

    MaJ
    Modification de certaines conditions et ajouts du launcher panel

--- src/fr/hdcarbone/epsicraft/LauncherFrame.java
+++ src/fr/hdcarbone/epsicraft/LauncherFrame.java
@@ -1,37 +1,42 @@
 package fr.hdcarbone.epsicraft;
 
 import javax.swing.JFrame;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+
+import fr.theshark34.openlauncherlib.launcher.util.WindowMover;
+import fr.theshark34.swinger.Swinger;
 
 @SuppressWarnings("serial")
 public class LauncherFrame extends JFrame{
 	
 	private static LauncherFrame instance;
+	private LauncherPanel launcherPanel;
 	
-	public LauncherFrame() {
+	public LauncherFrame(){
 		this.setTitle("EPSICraft V1 Launcher");
+		this.setSize(975, 625);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setUndecorated(true);
+		this.setIconImage(Swinger.getResource("icon.png"));
+		this.setContentPane(launcherPanel= new LauncherPanel());
+		
+		WindowMover mover = new WindowMover(this);
+		this.addMouseListener(mover);
+		this.addMouseMotionListener(mover);
+		
+		this.setVisible(true);
 	}
 	
 	public static void main(String[] args){
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Swinger.setSystemLookNFeel();
+		Swinger.setResourcePath("/fr/hdcarbone/epsicraft/resources/");
 	}
 	
 	public static LauncherFrame getInstance() {
 		return instance;
+	}
+	
+	public LauncherPanel getLauncherPanel() {
+		return this.launcherPanel;
 	}
 }
---
+++ src/fr/hdcarbone/epsicraft/LauncherPanel.java
@@ -0,0 +1,13 @@
+package fr.hdcarbone.epsicraft;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class LauncherPanel extends JPanel {
+	@Override
+	public void paintComponent(Graphics g){
+		
+	}
+}
